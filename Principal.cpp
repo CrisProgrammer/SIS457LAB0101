857cf28f3d13bdf7ea8a150287c07427506dc6c2
CrisProgrammer <rios.cristopheralvaro@gmail.com>
2022-04-12 00:30:33 -0400

    primer commit

---
+++ Principal.cpp
@@ -0,0 +1,19 @@
+// Principal.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Biografia de Cristopher Rios:\n--Cristopher nacio en Sucre-Bolivia el 12-09-2001.\n--Crecio en Sucre y estudio en el colegio Don Bosco.\n--Se graduo la promocion de 2019.\n--Hoy en dia estudia Ing. de Sistemas en la USFX.";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
